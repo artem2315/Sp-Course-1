7b2b3e9eb247953dabee4388ec30814791a2df4c
artem2315 <40527253+artem2315@users.noreply.github.com>
2020-04-29 19:58:13 +0300

    Add files via upload

---
+++ SP-Course-1/SysProgLab1/ConsoleApplication1/ConsoleApplication1/ConsoleApplication1.cpp
@@ -0,0 +1,72 @@
+﻿#include "pch.h"
+#include <iostream>
+#include "windows.h"
+#include "stdio.h"
+#include "string.h"
+#include <locale.h>
+
+void PrintError();
+void PrintSystemInfo();
+
+int main( int argc, char* argv[])
+{
+	setlocale(LC_ALL, "Russian");
+	printf("*************** KEYS ***************\n");
+	printf("-e - show error code and description\n");
+	printf("-s - show system information\n");
+	if (argc != 2)
+	{
+		printf_s("No input! Try again\n");
+	}
+	else
+	{
+		if (strcmp(argv[1], "-e") == 0)
+		{
+			PrintError();
+		}
+		else if (strcmp(argv[1], "-s") == 0)
+		{
+			PrintSystemInfo();
+		}
+		else
+		{
+			printf("No such key. Input the right one and try again!\n");
+		}
+	}
+	system("pause");
+	return 0;
+}
+
+void PrintError()
+{
+	MEMORYSTATUS memory_status;
+	GlobalMemoryStatus(&memory_status);
+	if (LocalAlloc(LPTR, memory_status.dwTotalPhys * (3)) == NULL) {
+		LPVOID error_message;
+		DWORD error_code = GetLastError();
+		FormatMessage(FORMAT_MESSAGE_ALLOCATE_BUFFER | FORMAT_MESSAGE_FROM_SYSTEM,
+		NULL, error_code,
+		MAKELANGID(LANG_NEUTRAL, SUBLANG_DEFAULT),
+		(LPTSTR)&error_message, 0, NULL);
+		printf("*************** ERROR ***************\n");
+		wprintf(L"Error code: 0x%x\n", error_code);
+		wprintf(L"About error: %s\n", (char*)error_message);
+	}
+}
+
+void PrintSystemInfo()
+{
+	SYSTEM_INFO system_info;
+	GetSystemInfo(&system_info);
+	printf("*********** SYSTEM INFO ***********\n");
+	printf("Oem Id: %u\n", system_info.dwOemId);
+	printf("The processor architecture: %u\n", system_info.wProcessorArchitecture);
+	printf("Reserverd member for future use: %u\n", system_info.wReserved);
+	printf("Page size: %u\n", system_info.dwPageSize);
+	printf("MIN application address: %lx\n", system_info.lpMinimumApplicationAddress);
+	printf("MAX application address: %lx\n", system_info.lpMaximumApplicationAddress);
+	printf("Active processor mask: %u\n", system_info.dwActiveProcessorMask);
+	printf("Number of processors: %u\n", system_info.dwNumberOfProcessors);
+	printf("Processor type: %u\n", system_info.dwProcessorType);
+	printf("Allocation granularity address: %u\n", system_info.dwAllocationGranularity);
+}
---
+++ SP-Course-1/SysProgLab1/ConsoleApplication2/ConsoleApplication2/ConsoleApplication2.cpp
@@ -0,0 +1,161 @@
+﻿#include "pch.h"
+#include <iostream>
+#include "windows.h"
+#include "stdio.h"
+#include "string.h"
+#include <locale.h>
+
+void ANSI_To_Unicode(LPCSTR);
+void Unicode_To_ANSI(LPCSTR);
+void ShowError();
+
+int main(int argc, char* argv[])
+{
+	setlocale(LC_ALL, "rus");
+	printf("Введите ключ необходимой операции:\n");
+	printf("\t* ключи исходной кодировки файла:\n\t\t-a – ANSI файл;\n \t\t-u – Unicode файл.\n");
+	printf("\t* Полный путь к файлу с занесенными данными.\n");
+	printf("Пример: -x 'C:\\path\\to\\file' \n");
+
+		if (argc != 3)
+			{
+				printf("Ошибка! При вводе данных!\n");
+				printf("\t* ключи исходной кодировки файла:\n\t\t-a – ANSI файл;\n \t\t-u – Unicode файл.\n");
+				printf("\t* Полный путь к файлу с занесенными данными.\n");
+				printf("Пример: -x 'C:\\path\\to\\file' \n");
+				return 0;
+			}
+		else
+		{
+			if (strcmp(argv[1], "-a") == 0)
+				ANSI_To_Unicode(argv[2]);
+			else if (strcmp(argv[1], "-u") == 0)
+				Unicode_To_ANSI(argv[2]);
+			else
+				printf("Ошибка ввода ключа\n");
+		}
+	return 0;
+}
+
+void ANSI_To_Unicode(LPCSTR fileName)
+{
+	WCHAR* unicodeBuff;
+	DWORD countRdChars, countWrWChars;
+	HANDLE handleSourceFile, handleDestinationFile;
+	CHAR ansiBuff[1024];
+	BOOL resultFile = true;
+	CHAR nameDestinationFile[] = "C:\\Users\\Acer\\Desktop\\3.2\\СП\\SP-Course\\SysProgLab1\\ConsoleApplication2\\ANSI_Text_To_Unicode.txt";
+	printf("\nРезультат можно увидеть -> %s\n", nameDestinationFile);
+
+			handleSourceFile = CreateFile(fileName, GENERIC_READ, FILE_SHARE_READ, NULL, OPEN_EXISTING, FILE_ATTRIBUTE_NORMAL, NULL);
+
+				if (handleSourceFile == INVALID_HANDLE_VALUE)
+					{
+						ShowError();
+						exit(EXIT_FAILURE);
+					}
+
+			handleDestinationFile = CreateFile(nameDestinationFile, GENERIC_WRITE, 0, NULL, OPEN_ALWAYS, FILE_ATTRIBUTE_NORMAL, NULL);
+
+				if (handleDestinationFile == INVALID_HANDLE_VALUE)
+					{
+						ShowError();
+						exit(EXIT_FAILURE);
+					}
+
+			while (((resultFile = ReadFile(handleSourceFile, &ansiBuff, sizeof(ansiBuff), &countRdChars, NULL)) != false)
+				&& (countRdChars != 0))
+					{
+						int sizeToAllocate = MultiByteToWideChar(CP_UTF8, 0, ansiBuff, countRdChars, NULL, 0);
+						unicodeBuff = (WCHAR*)calloc(sizeToAllocate, sizeof(WCHAR));
+						MultiByteToWideChar(CP_UTF8, 0, ansiBuff, countRdChars, unicodeBuff, sizeToAllocate);
+						if ((!WriteFile(handleDestinationFile, unicodeBuff, sizeToAllocate * sizeof(WCHAR), &countWrWChars, NULL)) ||
+							(countWrWChars != sizeToAllocate * sizeof(WCHAR)))
+								{
+									ShowError();
+									exit(EXIT_FAILURE);
+								}
+						free(unicodeBuff);
+					}
+
+				if (resultFile == false && countRdChars != 0)
+					{
+						ShowError();
+						exit(EXIT_FAILURE);
+					}
+
+			CloseHandle(handleSourceFile);
+			CloseHandle(handleDestinationFile);
+}
+
+void Unicode_To_ANSI(LPCSTR fileName)
+{
+	WCHAR* unicodeBuff;
+	DWORD countReadBytes, countWriteBytes;
+	HANDLE handleSourceFile, handleDestinationFile;
+	CHAR rawBytesBuff[1024];
+	CHAR* ansiBuff; 
+	BOOL resultFile = true;
+	CHAR nameDestinationFile[] = "C:\\Users\\Acer\\Desktop\\3.2\\СП\\SP-Course\\SysProgLab1\\ConsoleApplication2\\Unicode_Text_To_ANSI.txt";
+	printf("\nРезультат можно увидеть -> %s\n", nameDestinationFile);
+
+			handleSourceFile = CreateFile(fileName, GENERIC_READ, FILE_SHARE_READ, NULL, OPEN_EXISTING, FILE_ATTRIBUTE_NORMAL, NULL);
+				
+				if (handleSourceFile == INVALID_HANDLE_VALUE)
+					{
+						ShowError();
+						exit(EXIT_FAILURE);
+					}
+			handleDestinationFile = CreateFile(nameDestinationFile, GENERIC_WRITE, 0, NULL, OPEN_ALWAYS, FILE_ATTRIBUTE_NORMAL, NULL);
+					
+				if (handleDestinationFile == INVALID_HANDLE_VALUE)
+						{
+							ShowError();
+							exit(EXIT_FAILURE);
+						}
+							if (!ReadFile(handleSourceFile, &rawBytesBuff, 2, &countReadBytes, NULL))
+								{
+									ShowError();
+									exit(EXIT_FAILURE);
+								}
+
+			while (((resultFile = ReadFile(handleSourceFile, &rawBytesBuff, sizeof(rawBytesBuff), &countReadBytes, NULL)) != false)
+				&& (countReadBytes != 0))
+					{
+						int unicodeLenght = MultiByteToWideChar(CP_UTF8, 0, rawBytesBuff, countReadBytes, NULL, 0);
+						unicodeBuff = (WCHAR*)calloc(unicodeLenght, sizeof(WCHAR));
+						MultiByteToWideChar(CP_UTF8, 0, rawBytesBuff, countReadBytes, unicodeBuff, unicodeLenght);
+						int asciLenght = WideCharToMultiByte(CP_ACP, 0, unicodeBuff, unicodeLenght, NULL, 0, NULL, NULL);
+						ansiBuff = (CHAR*)calloc(asciLenght, sizeof(CHAR));
+						WideCharToMultiByte(CP_ACP, 0, unicodeBuff, unicodeLenght, ansiBuff, asciLenght, NULL, NULL);
+						if ((!WriteFile(handleDestinationFile, ansiBuff, asciLenght, &countWriteBytes, NULL)) || (countWriteBytes != asciLenght))
+							{
+								ShowError();
+								exit(EXIT_FAILURE);
+							}
+						free(unicodeBuff);
+						free(ansiBuff);
+					}
+
+			if (resultFile == false && countReadBytes != 0)
+				{
+					ShowError();
+					exit(EXIT_FAILURE);
+				}
+
+			CloseHandle(handleSourceFile);
+			CloseHandle(handleDestinationFile);
+}
+void ShowError()
+{
+	setlocale(LC_ALL, "rus");
+	LPVOID error_message;
+	DWORD error_code = GetLastError();
+	FormatMessage(FORMAT_MESSAGE_ALLOCATE_BUFFER | FORMAT_MESSAGE_FROM_SYSTEM,
+		NULL, error_code,
+		MAKELANGID(LANG_NEUTRAL, SUBLANG_DEFAULT),
+		(LPTSTR)&error_message, 0, NULL);
+	printf("*************** ERROR ***************\n");
+	wprintf(L"Error code: 0x%x\n", error_code);
+	wprintf(L"About error: %s\n", (char*)error_message);
+}
